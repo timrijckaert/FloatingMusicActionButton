769a532c44644ebda0a5e7bf01feb2815747a22c
Tim Rijckaert <timrijckaert@users.noreply.github.com>
2017-12-09 15:56:28 +0100

    Update FloatingMusicActionButton.kt

--- library/src/main/kotlin/be/rijckaert/tim/animatedvector/FloatingMusicActionButton.kt
+++ library/src/main/kotlin/be/rijckaert/tim/animatedvector/FloatingMusicActionButton.kt
@@ -3,27 +3,27 @@ package be.rijckaert.tim.animatedvector
 import android.content.Context
 import android.graphics.drawable.Animatable
 import android.graphics.drawable.Drawable
+import android.support.annotation.DrawableRes
 import android.support.design.widget.FloatingActionButton
 import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
 import android.view.View
-import be.rijckaert.tim.vector.sample.library.R
+import be.topradio.app.R
+import be.topradio.app.ui.widget.FloatingMusicActionButton.Mode.*
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.channels.actor
+import kotlinx.coroutines.experimental.channels.consumeEach
+import kotlinx.coroutines.experimental.delay
 
 class FloatingMusicActionButton : FloatingActionButton {
 
-    private val playToPauseDrawable: Drawable by lazy { ContextCompat.getDrawable(context, R.drawable.play_to_pause_animation) }
-    private val pauseToPlayDrawable: Drawable by lazy { ContextCompat.getDrawable(context, R.drawable.pause_to_play_animation) }
-    private val playToStopDrawable: Drawable by lazy { ContextCompat.getDrawable(context, R.drawable.play_to_stop_animation) }
-    private val stopToPlayDrawable: Drawable by lazy { ContextCompat.getDrawable(context, R.drawable.stop_to_play_animation) }
     private val maximumAnimationDuration by lazy { context.resources.getInteger(R.integer.play_button_animation_duration).toLong() }
 
-    private var listener: OnMusicFabClickListener? = null
-
-    var currentMode: Mode
-        private set
-    val currentDrawable: Drawable
-        get() {
-            return getAnimationDrawable()
+    var onMusicFabClickListener: OnMusicFabClickListener? = null
+    private var currentMode: Mode = PLAYPAUSE
+        set(value) {
+            field = value
+            setImageDrawable(field)
         }
 
     //<editor-fold desc="Chaining Constructors">
@@ -34,92 +34,50 @@ class FloatingMusicActionButton : FloatingActionButton {
                 attrs,
                 R.styleable.FloatingMusicActionButton,
                 0, 0)
-
         try {
-            currentMode = getMode(typedArray.getInteger(R.styleable.FloatingMusicActionButton_mode, 0))
+            currentMode = getMode(typedArray.getInteger(R.styleable.FloatingMusicActionButton_mode, PLAYPAUSE.styleableInt))
         } finally {
             typedArray.recycle()
         }
 
-        this.setOnClickListener {
-            playAnimation()
-            listener?.onClick(this)
-        }
-
-        this.setImageDrawable(currentDrawable)
+        this.setOnClickListener { eventActor.offer(it) }
     }
     //</editor-fold>
 
-    fun playAnimation() {
-        this.setImageDrawable(currentDrawable)
-        currentDrawable.startAsAnimatable {
-            setOppositeMode()
-        }
-    }
-
-    private fun setOppositeMode() {
-        currentMode = getOppositeMode()
-    }
-
-    @Synchronized
-    fun changeMode(mode: Mode) {
-        if (mode != currentMode) {
-            currentMode = mode
-
-            setOppositeMode()
-            playAnimation()
-        }
-    }
-
     //<editor-fold desc="Helpers">
-    private fun getMode(mode: Int): Mode = when (mode) {
-        0 -> Mode.PLAY_TO_PAUSE
-        1 -> Mode.PLAY_TO_STOP
-        2 -> Mode.PAUSE_TO_PLAY
-        3 -> Mode.STOP_TO_PLAY
-        else -> Mode.STOP_TO_PLAY
+    private fun setImageDrawable(mode: Mode) {
+        val animatedVector = ContextCompat.getDrawable(context, mode.drawableRes)
+        this.setImageDrawable(animatedVector)
     }
 
-    fun getOppositeMode(): Mode {
-        val position = if (currentMode.ordinal % 2 == 0) currentMode.ordinal + 1 else currentMode.ordinal - 1
-        return Mode.values()[position]
-    }
+    private fun getMode(styleableInt: Int): Mode = listOf(PLAYPAUSE, PAUSEPLAY, PLAYSTOP, STOPPLAY).first { it.styleableInt == styleableInt }
 
-    private fun getAnimationDrawable(): Drawable =
-            when (currentMode) {
-                Mode.PLAY_TO_PAUSE -> {
-                    playToPauseDrawable
-                }
-                Mode.PAUSE_TO_PLAY -> {
-                    pauseToPlayDrawable
-                }
-                Mode.PLAY_TO_STOP -> {
-                    playToStopDrawable
-                }
-                else -> {
-                    stopToPlayDrawable
-                }
-            }
-
-    fun setOnMusicFabClickListener(listener: OnMusicFabClickListener) {
-        this.listener = listener
-    }
-
-    enum class Mode(private val modeInt: Int, val isShowingPlayIcon: Boolean = false) {
-        PLAY_TO_PAUSE(0, true),
-        PAUSE_TO_PLAY(2),
-        PLAY_TO_STOP(1, true),
-        STOP_TO_PLAY(3)
-    }
-
-    private fun Drawable.startAsAnimatable(finally: () -> Unit = { }) {
-        if (this is Animatable) {
-            this.start()
-            this@FloatingMusicActionButton.postDelayed({
-                finally.invoke()
-            }, maximumAnimationDuration)
+    private val eventActor = actor<View>(UI) {
+        channel.consumeEach {
+            this@FloatingMusicActionButton.drawable.startAsAnimatable()
+            onMusicFabClickListener?.onClick(it)
+            delay(maximumAnimationDuration)
+            val oppositeMode = currentMode.getOppositeMode()
+            currentMode = oppositeMode
         }
     }
+
+    private fun Drawable.startAsAnimatable() = (this as Animatable).start()
+
+    sealed class Mode(val styleableInt: Int, @DrawableRes val drawableRes: Int) {
+        object PLAYPAUSE : Mode(0, R.drawable.play_to_pause_animation)
+        object PAUSEPLAY : Mode(1, R.drawable.pause_to_play_animation)
+        object PLAYSTOP : Mode(2, R.drawable.play_to_stop_animation)
+        object STOPPLAY : Mode(3, R.drawable.stop_to_play_animation)
+    }
+
+    private val opposites = mapOf(
+            PLAYPAUSE to PAUSEPLAY,
+            PAUSEPLAY to PLAYPAUSE,
+            PLAYSTOP to STOPPLAY,
+            STOPPLAY to PLAYSTOP
+    )
+    private fun Mode.getOppositeMode() = opposites[this]!!
 
     interface OnMusicFabClickListener {
         fun onClick(view: View)
